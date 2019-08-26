4e0d3b0a91ee87b819bf52fed8ace88c8d89b445
Anderson Duboc <andersonduboc@gmail.com>
2019-08-26 11:07:31 -0700

    new add

--- istio-demo.sh
+++ istio-demo.sh
@@ -6,7 +6,7 @@
 . demo-magic.sh
 clear
 
-# Script
+# Installing minikube
 p "brew cask install minikube"
 
 p "brew install docker-machine-drive-vmware"
@@ -15,20 +15,25 @@ p "minikube config set vm-driver vmware"
 
 p "minikube start --memory=16384 --cpus=4 --kubernetes-version=v1.4.2"
 
+# LoadBalancer 
 p "sudo minikube tunnel"
 
+# check kubernetes nodes
 p "kubectl get nodes"
 
+# install helm and add helm repo
 p "brew install kubernetes-helm"
 
 p "helm repo add istio.io https://storage.googleapis.com/istio-release/releases/1.2.4/charts/"
 
+# download istio release
 p "curl -L https://git.io/getLatestIstio | ISTIO_VERSION=1.2.4 sh -"
 
 p "cd istio-1.2.4"
 
 p "export PATH=$PWD/bin:$PATH"
 
+# deploy istio using helm with demo profile
 p "kubectl create namespace istio-system"
 
 p "helm template install/kubernetes/helm/istio-init --name istio-init --namespace istio-system | kubectl apply -f -"
@@ -38,3 +43,19 @@ p "kubectl get crds | grep 'istio.io\|certmanager.k8s.io' | wc -l"
 p "helm template install/kubernetes/helm/istio --name istio --namespace istio-system --values install/kubernetes/helm/istio/values-istio-demo.yaml | kubectl apply -f -"
 
 p "kubectl get svc -n istio-system"
+
+p "kubectl get pods -n istio-system"
+
+# deploy bookinfo app
+p "kubectl label namespace default istio-injection=enabled"
+
+p "kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml"
+
+# testing bookinfo app with curl
+p "kubectl exec -it $(kubectl get pod -l app=ratings -o jsonpath='{.items[0].metadata.name}') -c ratings -- curl productpage:9080/productpage | grep -o "<title>.*</title>""
+
+##  cleanup 
+p "helm template install/kubernetes/helm/istio --name istio --namespace istio-system --values install/kubernetes/helm/istio/values-istio-demo.yaml | kubectl delete -f -"
+
+p "kubectl delete namespace istio-system"
+
